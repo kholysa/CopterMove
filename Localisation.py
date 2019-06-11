0aed8355a524e98891f132495e557cfa37aa8e83
kholysa <kholysa@gmail.com>
2019-06-11 17:06:46 +0200

    Added comment clarifying what the method should return

--- Localisation.py
+++ Localisation.py
@@ -2,6 +2,7 @@ class Localisation:
 
     @staticmethod
     def GetPosition():
+        # Will read the text file and provide the most up to date localisation information
         x = 5
         y = 4
         return x, y
